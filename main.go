c9c6c1f485f3a7853fb85309e4e7ed324bb2a785
HorraceMuendo <horracemuendo34@gmail.com>
2023-01-06 23:15:28 +0300

    setting up handler functions

--- main.go
+++ main.go
@@ -7,12 +7,41 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello")
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNoContent)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+	fmt.Fprint(w, "hello wolrd")
+}
+func html(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+
+}
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
 
 func main() {
 
 	http.HandleFunc("/hello", hello)
 
-	log.Fatal(http.ListenAndServe(":3100", nil))
+	http.HandleFunc("/form", formHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":3100", nil); err != nil {
+		log.Fatal(err)
+	}
 }
