7510b8887cab2dddb3472a8d1dca4081c5ddde10
HorraceMuendo <horracemuendo34@gmail.com>
2023-01-07 21:57:20 +0300

    fixed the range error

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Passenger struct {
+type Passengers struct {
+	Id        string  `json:"id"`
 	Firstname string  `json:"firstname"`
 	Lastname  string  `json:"Lastname"`
 	Ticket    *Ticket `json:"ticket"`
@@ -19,15 +20,24 @@ type Ticket struct {
 	TicketNumber int `json:"ticketnumber"`
 }
 
-var passenger []Passenger
+var passengers []Passengers
 
 func getPassengers(w http.ResponseWriter, r *http.Request) {
 
-	//w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(passenger)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(passengers)
 
 }
 func getPassengerId(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	for _, passenger := range passengers {
+		if passenger.Id == params["id"] {
+			json.NewEncoder(w).Encode(passenger)
+			return
+		}
+	}
 
 }
 func updatePassenger(w http.ResponseWriter, r *http.Request) {
@@ -40,7 +50,10 @@ func deletePassenger(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router := mux.NewRouter()
 	//mock data
-	passenger = append(passenger, Passenger{Firstname: "horrace", Lastname: "you know", Ticket: &Ticket{TicketNumber: 10}})
+	passengers = append(passengers, Passengers{Id: "1", Firstname: "horrace", Lastname: "muendo", Ticket: &Ticket{TicketNumber: 10}})
+	passengers = append(passengers, Passengers{Id: "2", Firstname: "maggie", Lastname: "wambui", Ticket: &Ticket{TicketNumber: 13}})
+	passengers = append(passengers, Passengers{Id: "3", Firstname: "victor", Lastname: "muchui", Ticket: &Ticket{TicketNumber: 17}})
+	passengers = append(passengers, Passengers{Id: "4", Firstname: "fortune", Lastname: "timothy", Ticket: &Ticket{TicketNumber: 20}})
 
 	router.HandleFunc("/getPassengers", getPassengers).Methods("GET")
 	router.HandleFunc("/getPassenger/{id}", getPassengerId).Methods("GET")
